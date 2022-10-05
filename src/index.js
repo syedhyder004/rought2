55738220ae39d7714e003cb0be62081ac92a2ea4
syedhyder004 <syedhyder004@gmail.com>
2022-10-05 09:31:29 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const string = "hello hyder how are you";
+//const str1 = string.split(' ');
+console.log(string.split(" ").reverse().join(" "));
+// console.log(window.location.href);
+
+// const arr1 = [1];
+
+// console.log(arr1.push(2));
+// console.log(arr1.splice(arr1.length, 3, 55));
+// console.log([...arr1, 1111]);
+// console.log(Array.isArray(arr1));
+// console.log(typeof(arr1));
+// console.log(arr1);
+
+// const obj = [['cow', 'ambba'], ['cat', 'meow']];
+
+// console.log(Object.fromEntries(obj));
+// const obj1 = { key1: 'value1', key2: 'value2' };
+
+// const ar1 = Object.entries(obj1);
+// console.log(ar1);
